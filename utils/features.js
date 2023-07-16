04dabac4360e9f9caf552bff032025e9f95d9bf6
anupgithb <anup.dutta@zimtive.com>
2023-07-16 18:16:48 +0530

    changes6

--- utils/features.js
+++ utils/features.js
@@ -7,15 +7,16 @@ import User from "../models/user-schema.js";
     dotenv.config();
 
     export const cookieSetter = (res, token, set) => {
+        const domain = '.code-editor-deploy.onrender.com'; // Replace with your domain
         const serializedCookie = cookie.serialize('token', set ? token : "", {
-            path: '/',
-            httpOnly: true,
-            maxAge: set ? 24*60*60*1000 : 0,
-            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
-            secure: process.env.NODE_ENV === 'production',
-            domain: process.env.NODE_ENV === 'production' ? '.cyclic.sh' : undefined
-          }); 
-        res.setHeader('Set-Cookie',serializedCookie );
+          path: '/',
+          httpOnly: true,
+          maxAge: set ? 24 * 60 * 60 * 1000 : 0,
+          sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+          secure: process.env.NODE_ENV === 'production',
+          domain: process.env.NODE_ENV === 'production' ? domain : undefined
+        });
+        res.setHeader('Set-Cookie', serializedCookie);
         const setCookieHeader = res.getHeader('Set-Cookie');
     };
 
@@ -26,10 +27,21 @@ import User from "../models/user-schema.js";
 
     export const checkAuth = async (req) => {
         const cookie = req.headers.cookie;
-        // console.log(cookie);
-        if (!cookie) return null;
+        let tokenValue = null;
+        const cookieArray = cookie.split(';');
+
+        for (const cookie of cookieArray) {
+            const [key, value] = cookie.trim().split('=');
+            if (key === 'token') {
+              tokenValue = value;
+              break;
+            }
+          }
+
+        console.log(cookie);
+        if (!tokenValue) return null;
       
-        const token = cookie.split("=")[1];
+        const token = tokenValue;
       
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
       
